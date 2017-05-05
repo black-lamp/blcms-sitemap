82de261350067c3272c007541422c4c3a45dc878
Вадим <guts.vadim@gmail.com>
2017-05-05 13:02:56 +0300

    console module default route

--- console/Module.php
+++ console/Module.php
@@ -4,7 +4,7 @@ namespace bl\cms\sitemap\console;
 class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'bl\cms\sitemap\console\controllers';
-    public $defaultRoute = 'sitemap';
+    public $defaultRoute = 'sitemap/generate';
 
     public function init()
     {
